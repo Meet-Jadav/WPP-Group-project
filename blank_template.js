b3319b5fa02d123f5c9b93a5ecf482e3d5e8127c
Meet-Jadav <jadavmeet2006@gmail.com>
2025-04-16 14:53:19 +0530

    Update blank_template.js
     Added the text changeer functionality.

--- blank_template.js
+++ blank_template.js
@@ -9,14 +9,28 @@ elements.forEach(function (element) {
                 element.style.height = '160px';
             });
             element.classList.remove('expanded');
+
+            // Restore the original text
+            let textElement = element.querySelector('.text');
+            if (textElement) {
+                textElement.textContent = textElement.getAttribute('data-original-text');
+            }
         } else {
             let currentHeight = element.scrollHeight;
             element.style.height = '160px';
-      
             requestAnimationFrame(() => {
                 element.style.height = currentHeight + 'px';
             });
             element.classList.add('expanded');
+
+            // Change the text
+            let textElement = element.querySelector('.text');
+            if (textElement) {
+                if (!textElement.getAttribute('data-original-text')) {
+                    textElement.setAttribute('data-original-text', textElement.textContent);
+                }
+                textElement.textContent = 'This is the new text!';
+            }
         }
     });
 
