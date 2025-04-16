bfe1661a9be07967c8d9d7d33122433efc974ddf
Meet-Jadav <jadavmeet2006@gmail.com>
2025-04-16 21:50:51 +0530

    Changed the click functionality

--- blank_template.js
+++ blank_template.js
@@ -9,31 +9,16 @@ elements.forEach(function (element) {
                 element.style.height = '160px';
             });
             element.classList.remove('expanded');
-
-            // Restore the original text
-            let textElement = element.querySelector('.text');
-            if (textElement) {
-                textElement.textContent = textElement.getAttribute('data-original-text');
-            }
         } else {
             let currentHeight = element.scrollHeight;
             element.style.height = '160px';
+      
             requestAnimationFrame(() => {
                 element.style.height = currentHeight + 'px';
             });
             element.classList.add('expanded');
-
-            // Change the text
-            let textElement = element.querySelector('.text');
-            if (textElement) {
-                if (!textElement.getAttribute('data-original-text')) {
-                    textElement.setAttribute('data-original-text', textElement.textContent);
-                }
-                textElement.textContent = 'This is the new text!';
-            }
         }
     });
 
     element.style.transition = 'height 0.5s ease-in-out';
 });
-
