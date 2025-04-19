70408f6487d0e08fd7574b8eac4756b1f2981ec3
Meet-Jadav <jadavmeet2006@gmail.com>
2025-04-19 12:18:50 +0530

    Final 2013 & 2014 template

--- Data By Years/Script.js
+++ Data By Years/Script.js
@@ -1,24 +1,34 @@
 var elements = document.querySelectorAll('.element');
 
 elements.forEach(function (element) {
-    element.addEventListener('click', function () {
-        if (element.classList.contains('expanded')) {
-            let currentHeight = element.scrollHeight;
-            element.style.height = currentHeight + 'px';
-            requestAnimationFrame(() => {
-                element.style.height = '160px';
-            });
-            element.classList.remove('expanded');
-        } else {
-            let currentHeight = element.scrollHeight;
-            element.style.height = '160px';
-      
-            requestAnimationFrame(() => {
-                element.style.height = currentHeight + 'px';
-            });
-            element.classList.add('expanded');
-        }
-    });
+  const hiddenContent = element.querySelector('.hide'); // get the hidden part inside
 
-    element.style.transition = 'height 0.5s ease-in-out';
+  element.addEventListener('click', function () {
+
+    if (element.classList.contains('expanded')) {
+      // Collapse
+      let currentHeight = element.scrollHeight;
+      element.style.height = currentHeight + 'px';
+      requestAnimationFrame(() => {
+        element.style.height = '160px';
+      });
+      element.classList.remove('expanded');
+
+      if (hiddenContent) hiddenContent.style.visibility = 'hidden';
+
+    } else {
+      // Expand
+      let currentHeight = element.scrollHeight;
+      element.style.height = '160px';
+
+      requestAnimationFrame(() => {
+        element.style.height = currentHeight + 'px';
+      });
+      element.classList.add('expanded');
+
+      if (hiddenContent) hiddenContent.style.visibility = 'visible';
+    }
+  });
+
+  element.style.transition = 'height 0.5s ease-in-out';
 });
